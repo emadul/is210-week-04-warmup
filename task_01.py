c8df82e9e26f99a9e89fb84a09d9cc53864623bc
Emadul Islam <phemadul@gmail.com>
2015-02-20 05:07:04 +0000

    added raw_input

---
+++ task_01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""RAW INPUT."""
+MY_ANSWER = raw_input('What is your name? ')
---
+++ task_02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""raw input type int"""
+MY_INTEGER = raw_input('What is the answer to the life the universe and everything? ')
+MY_INTEGER = int(MY_INTEGER)
