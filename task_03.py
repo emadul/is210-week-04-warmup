d0b4102e0c97648ae170ff8ed447239c082e4ea9
Emadul Islam <phemadul@gmail.com>
2015-02-21 00:48:38 +0000

    added logical operator

--- task_03.py
+++ task_03.py
@@ -6,3 +6,7 @@ EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+sacrifice = (EXPENSE <= MAX_EXPENSE == GET_OUT_ALIVE ) or (EXPENSE >= MAX_EXPENSE == MAX_EXPENSE)
+
+print sacrifice
---
+++ task_05.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""if statement."""
+
+BP_STATUS = raw_input('What is your blood pressure? ')
+
+if BP_STATUS > 89:
+
+    print 'Low'
+ 
+elif BP_STATUS >= 90: 
+    print 'IDEAL!'
+
+else BP_STATUS >=160
+
+ 
+
+print '{}'.format('Your status is currently: ')
